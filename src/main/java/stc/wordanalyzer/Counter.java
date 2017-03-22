01ab80ba1c3dd27985065501cb31f72d0619c928
Yongyao Jiang <J.yongyao11@gmail.com>
2017-03-22 16:52:45 -0400

    Code cleanup

--- src/main/java/stc/wordanalyzer/Counter.java
+++ src/main/java/stc/wordanalyzer/Counter.java
@@ -24,127 +24,145 @@ import org.elasticsearch.search.SearchHit;
 
 public class Counter {
 
-	public Counter() {
-		// TODO Auto-generated constructor stub
-	}
+  public Counter() {
+  }
 
-	protected Client makeClient() throws IOException {
-		String clusterName = "elasticsearch";
-		String host = "127.0.0.1";
-		int port = 9300;
+  protected Client makeClient() throws IOException {
+    String clusterName = "elasticsearch";
+    String host = "127.0.0.1";
+    int port = 9300;
 
-		Settings.Builder settingsBuilder = Settings.settingsBuilder();
-		settingsBuilder.put("cluster.name", clusterName);
-		Settings settings = settingsBuilder.build();
+    Settings.Builder settingsBuilder = Settings.settingsBuilder();
+    settingsBuilder.put("cluster.name", clusterName);
+    Settings settings = settingsBuilder.build();
 
-		Client client = null;
-		TransportClient transportClient = TransportClient.builder().settings(settings).build();
+    Client client = null;
+    TransportClient transportClient = TransportClient.builder().settings(settings).build();
 
-		transportClient.addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName(host), port));
-		client = transportClient;
+    transportClient.addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName(host), port));
+    client = transportClient;
 
-		return client;
-	}
+    return client;
+  }
 
-	public static void main(String[] args) {
+  public static void main(String[] args) {
 
-		Counter counter = new Counter();
-	
-		Client client = null;
-		try {
-			client = counter.makeClient();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+    if(args.length<1)
+    {
+      System.out.println("Please input the output path!");
+      return;
+    }
 
-		if (client != null) {
-			System.out.println("you made it");
-			counter.statisticTerms(client);
-		}
-	}
+    Counter counter = new Counter();
 
-	public void statisticTerms(Client client) {
+    Client client = null;
+    try {
+      client = counter.makeClient();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
 
-		Map<String, Integer> totalTermCounts = new HashMap<String, Integer>();
+    if (client != null) {
+      System.out.println("ES client created successfully.");
+      counter.sumTerms(client, args[0]);
+    }
+  }
 
-		String index = "nutch";
-		String type = "doc";
-		SearchResponse scrollResp = client.prepareSearch(index).setTypes(type).setQuery(QueryBuilders.matchAllQuery())
-				.setScroll(new TimeValue(60000)).setSize(100).execute().actionGet();
+  public void sumTerms(Client client, String outpath) {
 
-		while (true) {
-			for (SearchHit hit : scrollResp.getHits().getHits()) {
-				Map<String, Object> result = hit.getSource();
+    Map<String, Integer> totalCounts = new HashMap<String, Integer>();
 
-				Set<String> pageTerms = new HashSet<String>();
-				String inlinks = (String) result.get("anchor_inlinks");
-				String outlinks = (String) result.get("anchor_outlinks");
-				
-				String allLinks = inlinks + "&&" + outlinks;
-				String[] linksTemrs = allLinks.split("&&");
-				for (String term : linksTemrs) {
-					term = term.trim();
-					if(!term.isEmpty()){
-						pageTerms.add(term);
-					}
-				}
-				
-				for (String term : pageTerms) {
-					if (totalTermCounts.containsKey(term)) {
-						int count = totalTermCounts.get(term);
-						totalTermCounts.put(term, count + 1);
-					} else {
-						totalTermCounts.put(term, 1);
-					}
-				}
-			}
-			scrollResp = client.prepareSearchScroll(scrollResp.getScrollId()).setScroll(new TimeValue(600000)).execute()
-					.actionGet();
-			if (scrollResp.getHits().getHits().length == 0) {
-				break;
-			}
-		}
+    String index = "nutch";
+    String type = "doc";
+    SearchResponse scrollResp = client.prepareSearch(index).setTypes(type).setQuery(QueryBuilders.matchAllQuery())
+        .setFetchSource(null, new String[]{"content"})
+        .setScroll(new TimeValue(60000)).setSize(100).execute().actionGet();
 
-		// write to file
-		Map<String, Integer> sortedTermCounts = this.sortByValue(totalTermCounts);
-		String fileName = "D:/statistic.txt";
-		File file = new File(fileName);
-		if (file.exists()) {
-			file.delete();
-		}
-		try {
-			file.createNewFile();
-		} catch (IOException e2) {
-			e2.printStackTrace();
-		}
+    while (true) {
+      for (SearchHit hit : scrollResp.getHits().getHits()) {
+        Map<String, Object> result = hit.getSource();
 
-		FileWriter fw;
-		BufferedWriter bw;
-		try {
-			fw = new FileWriter(file.getAbsoluteFile());
-			bw = new BufferedWriter(fw);
+        Set<String> pageTerms = new HashSet<String>();
+        String inlinks = (String) result.get("anchor_inlinks");
+        String outlinks = (String) result.get("anchor_outlinks");
+        String title = (String) result.get("title");
 
-			for (String term : sortedTermCounts.keySet()) {
-				bw.write(term + " " + sortedTermCounts.get(term) + "\n");
-			}
+        String text = inlinks + "&&" + outlinks + "&&" + title;
+        String[] linksTemrs = text.split("&&");
+        for (String term : linksTemrs) {
+          term = term.trim();
+          if(!term.isEmpty()){
+            pageTerms.add(term);
+          }
+        }
 
-			bw.close();
+        for (String term : pageTerms) {
+          if (totalCounts.containsKey(term)) {
+            int count = totalCounts.get(term);
+            totalCounts.put(term, count + 1);
+          } else {
+            totalCounts.put(term, 1);
+          }
+        }
+      }
 
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public static <K, V extends Comparable<? super V>> Map<K, V> sortByValue(Map<K, V> map) {
-	    return map.entrySet()
-	              .stream()
-	              .sorted(Map.Entry.comparingByValue(Collections.reverseOrder()))
-	              .collect(Collectors.toMap(
-	                Map.Entry::getKey, 
-	                Map.Entry::getValue, 
-	                (e1, e2) -> e1, 
-	                LinkedHashMap::new
-	              ));
-	}
+      scrollResp = client.prepareSearchScroll(scrollResp.getScrollId()).setScroll(new TimeValue(600000)).execute()
+          .actionGet();
+      if (scrollResp.getHits().getHits().length == 0) {
+        break;
+      }
+    }
+
+    // write and save to file
+    Map<String, Integer> sortedCounts = Counter.sortByValue(totalCounts);
+    File file = new File(outpath);
+    if (file.exists()) {
+      file.delete();
+    }
+    try {
+      file.createNewFile();
+    } catch (IOException e2) {
+      e2.printStackTrace();
+    }
+
+    FileWriter fw = null;
+    BufferedWriter bw = null;
+    try {
+      fw = new FileWriter(file.getAbsoluteFile());
+      bw = new BufferedWriter(fw);
+
+      for (String term : sortedCounts.keySet()) {
+        bw.write(term + " " + sortedCounts.get(term) + "\n");
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    }finally{
+      try{      
+        if(bw!=null)
+        {
+          bw.close();
+        }
+        
+        if(fw!=null)
+        {
+          fw.close();
+        }
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+
+    }
+  }
+
+  public static <K, V extends Comparable<? super V>> Map<K, V> sortByValue(Map<K, V> map) {
+    return map.entrySet()
+        .stream()
+        .sorted(Map.Entry.comparingByValue(Collections.reverseOrder()))
+        .collect(Collectors.toMap(
+            Map.Entry::getKey, 
+            Map.Entry::getValue, 
+            (e1, e2) -> e1, 
+            LinkedHashMap::new
+            ));
+  }
 }
